c294e41788be040d7b0a8bb4537e4bc17f4b0371
code-yasharrya <Yasharya.est2005@gmail.com>
2026-09-23 19:05:41 +0530

    Add data types and operators practice examples

---
+++ basics/data-types-operators/practice-examples.py
@@ -0,0 +1,39 @@
+# 1. Addition of two numbers
+a=1
+ab=1002
+print (a+ab)
+
+
+# 2. Find the remainder when one number is divided by another
+a=35 
+b=5 
+print("reminder when a is divided by b is", a % b)
+
+
+# 3. Take input from the user and check its data type
+a = input("enter the value of a: ")
+print(type(a))
+
+
+# 4. Check whether a is greater than b
+a=int(input("enter number 1: "))
+b=int(input("enter number 2: "))
+print("a is greater than b is", a>b)
+
+
+# 5. Calculate the average of two numbers
+a=int(input("enter number 1: "))
+b=int(input("enter number 2: "))
+print("the average of these two number is",(a+b)/2)
+
+
+# 6. Calculate the square of a number
+a=int(input("enter your number : "))
+print("the square of the number is",a**2)
+
+
+# 7. Convert a string value into a float and check its type
+a="31.32"
+b= float(a)# a but the type should be float
+t=type(b)
+print(t)
