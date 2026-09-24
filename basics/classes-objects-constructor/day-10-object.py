68273cf33292102a5cedd0b46b34b9bffb2c1891
code-yasharrya <Yasharya.est2005@gmail.com>
2026-09-24 17:27:38 +0530

    Add employee class with instance and class attributes

---
+++ basics/classes-objects-constructor/day-10-object.py
@@ -0,0 +1,22 @@
+class employee: 
+    language="python" #this is a class attributes
+    salary = 1200000
+yash = employee()
+yash.name="yash rajesh aarya\n"# this is instance attributes 
+yash.language = "marathi\n"
+print(yash. language,yash .salary)
+
+
+# instance attribute take prefference over class attribute during  assingment & retrival
+
+# self
+class employee: 
+    language="python" #this is a class attributes
+    salary = 1200000
+yash = employee()
+yash.name="yash rajesh aarya\n"# this is instance attributes 
+yash.language = "marathi\n"
+print(yash. language,yash .salary)
+
+
+# instance attribute take prefference over class attribute during  assingment & retrival
