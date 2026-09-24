eebcdcf2ad4c769c91e3239811aa5427045fee5b
code-yasharrya <Yasharya.est2005@gmail.com>
2026-09-24 16:38:06 +0530

    Update day-07-loops.py

--- basics/loops/day-07-loops.py
+++ basics/loops/day-07-loops.py
@@ -61,3 +61,63 @@ i=0
 while(i<45):
     print(i)
     i+=1
+
+# practice code
+
+# 1)
+n=int(input("enter the number\n"))
+for i in range(1,11):
+    print(f"{n}*{i}={n*i}")
+
+# 2)
+l = ("yash","yashwant","yashoda","karan","jigra")
+for name in l:
+    if(name.startswith("j")):
+        print(f"hellow {name}")
+# 3)
+n=int(input("enter the number\n"))
+
+for i in range(2,n):
+    if(n%i)==0:
+        print("number is not prime")
+        break
+
+else:
+    print("number is prime")
+
+# 4)
+n=int(input("enter the number\n"))
+product=1
+for i in range(1, n+1):
+    product=product*i
+print(f"the factorial of {n} is {product}")
+
+# 5)
+n=int(input("enter the number\n"))
+for i in range(1,n+1):
+    print(" "*(n-i),end="")
+    print("*"*(2*i-1),end="")
+    print("")
+ 
+ # 6)
+n=int(input("enter the number\n"))
+for i in range(1,n+1):
+    print("*"*i,end="")
+    print("")
+ # 7)
+n=int(input("enter the number\n"))
+for i in range(1,n+1):
+    if(i==1 or i==n):
+        print("*"*n,end="")
+    else:
+        print("*",end="")
+        print(" "*(n-2),end="")
+        print("*",end="")
+    print("")
+ 
+ # 8)
+
+n=int(input("enter the number\n"))
+for i in range(1,11):
+    print(f"{n}*{11-i}={n*(11-i)}")
+ 
