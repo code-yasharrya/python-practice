67e66bd8555a84b2eff51249c1d626adf6bf1b9b
code-yasharrya <Yasharya.est2005@gmail.com>
2026-09-24 18:10:21 +0530

    module

---
+++ basics/advance-python/module.py
@@ -0,0 +1,9 @@
+def myfunc():
+    print("hellow world")
+
+
+if  __name__ == "__main__":
+    # if this code is directly executed by running the file its present in
+    print("we are directly running this code")
+    myfunc()
+    print(__name__)
