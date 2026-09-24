72fed0b8a5c7a2df81a366481ac5e7db06d644d6
code-yasharrya <Yasharya.est2005@gmail.com>
2026-09-24 18:04:09 +0530

    Add exception handling for division by zero

---
+++ basics/advance-python/raising-exceptions.py
@@ -0,0 +1,7 @@
+a=int(input("enter your first number"))
+b=int(input("enter your second number "))
+if(b,a==0):
+    raise zeroDivisionError("hey our program is not meant to divide number by zero")
+
+else:
+    print(f"the division a/b is {a/b}")
